268e925f7782846b6d541c3a8f670fc18246dbcf
Sherri Lee <slpmssg@yahoo.com>
2021-09-10 18:20:44 -0400

    1

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+t = np.linspace(-np.pi,np.pi)
+
+plt.plot(t, np.sin(t), marker="+", label="sin", linestyle="-.")
+plt.plot(t, np.cos(t), marker="^", label="cos", linestyle=":")
+
+plt.title("sin & cos graph")
+plt.xlabel("angle")
+plt.ylabel("sin & cos value")
+plt.legend()
+plt.show()
